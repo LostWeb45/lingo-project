f3d3a13c9fa510960f78352d12e482492909333b
LostWeb45 <kostik052005@gmail.com>
2025-04-09 11:02:48 +0300

    Структура страниц

---
+++ app/(admin)/layout.tsx
@@ -0,0 +1,14 @@
+import type { Metadata } from "next";
+import "../globals.css";
+
+export const metadata: Metadata = {
+  title: "Lingo | Главная страница",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return <main>Админский лаяут{children}</main>;
+}

51154e07f2dd9caf84503cd56d45835985b0ce58
LostWeb45 <kostik052005@gmail.com>
2025-05-02 15:36:51 +0300

    Доделаный футер

--- components/shared/footer.tsx
+++ components/shared/footer.tsx
@@ -13,34 +13,42 @@ export const Footer: React.FC<Props> = ({ className }) => {
         className
       )}
     >
-      {/* <div className="absolute text-[100px] left-0 rotate-90 [font-family:var(--font-montserrat)]">
+      <div className="absolute text-[100px] left-[-90px] font-semibold opacity-10 rotate-90 [font-family:var(--font-montserrat)]">
         LinGo
-      </div> */}
+      </div>
       <div className="flex gap-[150px]">
-        <div className="flex flex-col gap-[12px]">
-          <div className="text-[19px] text-[#333333] font-bold">Помощь</div>
-          <div className="text-[16px] text-[#333333] font-medium">
+        <div className="flex flex-col gap-[10px] font-medium">
+          <div className="text-[19px] text-[#333333]">Помощь</div>
+          <div className="text-[16px] text-[#585858] font-medium">
             konstintin@gmail.com
           </div>
-          <div className="text-[16px] text-[#333333] font-medium">
+          <div className="text-[16px] text-[#585858] font-medium">
             kostik052005@gmail.com
           </div>
         </div>
-        <div>
+        <div className="flex flex-col gap-[10px]">
           <div className="text-[19px] text-[#333333] font-medium">
             О компании
           </div>
-          <div>Пользовательское соглашение</div>
+          <div className="text-[16px] text-[#585858] font-medium ">
+            Пользовательское соглашение
+          </div>
         </div>
-        <div>
+        <div className="flex flex-col gap-[10px]">
           <div className="text-[19px] text-[#333333] font-medium">
             Наши контакты
           </div>
-          <div>konstintin@gmail.com</div>
-          <div>г. Санкт-Петербург</div>
+          <div className="text-[16px] text-[#585858] font-medium ">
+            konstintin@gmail.com
+          </div>
+          <div className="text-[16px] text-[#585858] font-medium ">
+            г. Санкт-Петербург
+          </div>
         </div>
       </div>
-      <div className="absolute bottom-3">© ООО «Lingo» 2025</div>
+      <div className="absolute bottom-3 text-[18px] [font-family:var(--font-montserrat)]">
+        © ООО «Lingo» 2025
+      </div>
     </footer>
   );
 };

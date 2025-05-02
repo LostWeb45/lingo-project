984131b614cf0871669304daab3dbb5932fdf65d
LostWeb45 <kostik052005@gmail.com>
2025-05-02 13:14:35 +0300

    Карточка с выводом создателя

--- components/shared/event-card.tsx
+++ components/shared/event-card.tsx
@@ -1,7 +1,9 @@
 import { Event } from "@prisma/client";
-import { Button } from "../ui";
+import { Avatar, Button } from "../ui";
 import { format, addMinutes, parse } from "date-fns";
 import { ru } from "date-fns/locale";
+import { AvatarFallback, AvatarImage } from "../ui/avatar";
+import Link from "next/link";
 
 interface EventCardProps {
   event: Event;
@@ -19,6 +21,8 @@ const getFormattedDateTime = (date: Date, time: string, duration: number) => {
 };
 
 export const EventCard: React.FC<EventCardProps> = ({ event }) => {
+  console.log(event);
+
   return (
     <div>
       <div className="h-[200px] overflow-hidden">
@@ -34,7 +38,18 @@ export const EventCard: React.FC<EventCardProps> = ({ event }) => {
       </div>
       <div className="flex flex-col gap-[10px] bg-[#F5F6FA] p-[10px]">
         <div className="flex items-center justify-between">
-          <div>Пользователь</div>
+          <Link className="flex justify-center items-center gap-3" href={"/"}>
+            <Avatar className="cursor-pointer  w-[30px] h-[30px] hover:opacity-90 transition-opacity">
+              <AvatarImage src={event.createdBy.image ?? undefined} />
+              <AvatarFallback className="bg-[white]">
+                {event.createdBy.name?.charAt(0) ?? "П"}
+              </AvatarFallback>
+            </Avatar>
+            <p className="text-[#2E1A1A] text-[13px] hover:text-[#3A5F9D] transition-colors">
+              {event.createdBy.name?.split(" ")[0]}
+            </p>
+          </Link>
+
           <div>16+</div>
         </div>
         <h3 className="text-[19px] font-semibold">{event.title}</h3>
